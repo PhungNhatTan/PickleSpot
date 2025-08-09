7b268e6fdc40e029b4220eb54fb89c6cddcd4294
unknown <phungnhattan0305@gmail.com>
2025-08-09 22:02:03 +0700

    feat: Project initialization

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+//Route
+app.get("/", (req, res) => {
+    res.send("Welcome to the node server");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
