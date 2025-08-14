c376e4dfd406779e993d32238bd1044c77c19c39
unknown <phungnhattan0305@gmail.com>
2025-08-14 22:43:44 +0700

    feat!: general server-client connection config

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
-const express = require('express');
-const app = express();
-const PORT = 5000;
+const express = require("express");
+const path = require("path");
 
-//Route
-app.get("/", (req, res) => {
+const PORT = 5000;
+const app = express();
+
+app.get("/api/message", (req, res) => {
     res.send("Welcome to the node server");
 });
+
+if (process.env.NODE_ENV === "production") {
+  // Serve static files from client/dist in production
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  // Serve index.html for any unknown routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
+} else {
+  console.log("⚡ Development mode: not serving dist folder");
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
