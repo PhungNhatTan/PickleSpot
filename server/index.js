e08088e5b853abed859f405586036cdd1da996e4
unknown <phungnhattan0305@gmail.com>
2025-08-15 10:36:21 +0700

    feat: prepare project for deployment

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || '*'
+}));
 
 app.get("/api/message", (req, res) => {
     res.send("Welcome to the node server");
